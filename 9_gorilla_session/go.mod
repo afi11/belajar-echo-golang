281cb438bb3ede2f1f9de0bfe75b9a136a65996f
afif.fatakhul <fatakhulafi11@gmail.com>
2022-12-23 21:43:08 +0700

    Gorilla Session

---
+++ 9_gorilla_session/go.mod
@@ -0,0 +1,25 @@
+module 9_gorilla_session
+
+go 1.18
+
+require github.com/antonlindstrom/pgstore v0.0.0-20220421113606-e3a6e3fed12a
+
+require (
+	github.com/labstack/echo v3.3.10+incompatible // indirect
+	github.com/labstack/gommon v0.4.0 // indirect
+	github.com/mattn/go-colorable v0.1.11 // indirect
+	github.com/mattn/go-isatty v0.0.14 // indirect
+	github.com/valyala/bytebufferpool v1.0.0 // indirect
+	github.com/valyala/fasttemplate v1.2.1 // indirect
+	golang.org/x/crypto v0.4.0 // indirect
+	golang.org/x/net v0.3.0 // indirect
+	golang.org/x/sys v0.3.0 // indirect
+	golang.org/x/text v0.5.0 // indirect
+)
+
+require (
+	github.com/gorilla/context v1.1.1
+	github.com/gorilla/securecookie v1.1.1 // indirect
+	github.com/gorilla/sessions v1.2.1 // indirect
+	github.com/lib/pq v1.10.5 // indirect
+)
---
+++ 9_gorilla_session/main.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/antonlindstrom/pgstore"
+	"github.com/gorilla/context"
+	"github.com/labstack/echo"
+)
+
+const SESSION_ID = "id"
+
+func newPostgresStore() *pgstore.PGStore {
+	url := "postgres://fatakhul:1234@127.0.0.1:5432/fatakhul?sslmode=disable"
+	authKey := []byte("my-auth-key-very-secret")
+	encryptionKey := []byte("my-encryption-key-very-secret123")
+
+	store, err := pgstore.NewPGStore(url, authKey, encryptionKey)
+	if err != nil {
+		log.Println("ERROR", err)
+		os.Exit(0)
+	}
+
+	return store
+}
+
+func main() {
+	store := newPostgresStore()
+
+	e := echo.New()
+
+	e.Use(echo.WrapMiddleware(context.ClearHandler))
+
+	e.GET("/set", func(ctx echo.Context) error {
+		session, _ := store.Get(ctx.Request(), SESSION_ID)
+		session.Values["message1"] = "hello"
+		session.Values["message2"] = "world"
+		session.Save(ctx.Request(), ctx.Response())
+
+		return ctx.Redirect(http.StatusTemporaryRedirect, "/get")
+	})
+
+	e.GET("/get", func(ctx echo.Context) error {
+		session, _ := store.Get(ctx.Request(), SESSION_ID)
+
+		if len(session.Values) == 0 {
+			return ctx.String(http.StatusOK, "empty result")
+		}
+
+		return ctx.String(http.StatusOK, fmt.Sprintf(
+			"%s %s",
+			session.Values["message1"],
+			session.Values["message2"],
+		))
+	})
+
+	e.GET("/delete", func(ctx echo.Context) error {
+		session, _ := store.Get(ctx.Request(), SESSION_ID)
+		session.Options.MaxAge = -1
+		session.Save(ctx.Request(), ctx.Response())
+
+		return ctx.Redirect(http.StatusTemporaryRedirect, "/get")
+	})
+
+	e.Logger.Fatal(e.Start(":9000"))
+}
